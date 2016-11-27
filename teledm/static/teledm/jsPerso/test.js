a99cd96ca43bef274f39c3eee838bc462c1ee4a0
sebastien <merletsebastien@yahoo.fr>
2016-11-27 14:51:19 +0100

    2016-11-27

--- teledm/static/teledm/jsPerso/test.js
+++ teledm/static/teledm/jsPerso/test.js
@@ -1,6 +1,5 @@
 const ROOT = "http://localhost:8080/thredds";
-
-var tabCatalog = []
+var resoTemp = [['d','quotidien'],['w','hebdomadaire'], ['m','mensuel'], ['t','trimestriel']];
 var lstInfos = {
     date:"",
     param:"",
@@ -20,55 +19,6 @@ var lstInfos = {
     colorbarBand:'',
     opacity:''
 };
-
-
-
-function readCat(selected, selector){
-    if (selected == ''){
-        var URLCat = ROOT + "/catalog.xml";
-    }
-    else {
-        
-        var URLCat = ROOT + "/" + selected + "/catalog.xml";
-    }
-    var titre = [];
-    var IDS = [];
-    $.ajax( {
-				type: "GET",
-				url: URLCat,
-				dataType: "xml",
-				async: false,
-				success: function(xml) {
-        				if($(xml).find('catalogRef')!=0)  //Si catalogue(s) présent
-        				{
-        					$(xml).find('catalogRef').each( function(){  //Pour tout les catalogues
-        						titre.push($(this).attr('xlink:title'));   //Titre du catalogue
-        						var href= $(this).attr('xlink:href');   //Lien du catalogue
-        						href=href.replace("/thredds/catalog","");  //Modification du string servant de lien
-        						tabCatalog.push(href);   //Création de l'URL du catalogue
-        						//createNode(cat, titre, titre, "first");  //Création du noeud dans le tree
-        						//$("#listeCatMap").jstree('disable_node', titre);
-        						//parseRec(URLCat,"#"+titre);  //Parse le catalogue
-        					})
-                            setSelect(titre, selector);
-        				}
-                            if($(xml).find('dataset')!=0)   //Si data(s) présente(s)
-                            {
-                                alert('dataset');
-        					$(xml).find('dataset').each( function(){  //Pour toutes les datas
-                                       if($(this).attr('urlPath')){
-                                            alert('url');
-                                        }
-        						//var datasetName= $(this).attr('name');  //Nom du dataset
-        						//var urlDataset= $(this).attr('urlPath');  //URL du dataset
-        						//createNode(cat, datasetName, datasetName, "last");  //Création du noeud
-        					}) //Fin each
-                                
-        				}//Fin if
-				}//Fin success AJAX
-			})//Fin AJAX
-    alert(IDS);
-}
 
 
 function setSelect(array, bx){
@@ -102,6 +52,7 @@ function setForm(){
         //charge les choix de capteur
         createURL(this.value, selectSource1[1]);
         };
+    // choix du capteur
     selectSource1[1].onchange =  function(){
         //reinitialise les menus deroulants
         resetSelect(selectSource1, 2);
@@ -110,6 +61,7 @@ function setForm(){
         //charge les choix de produit
         createURL(this.value, selectSource1[2]);
         };
+    // choix du produit
     selectSource1[2].onchange =  function(){
         //reinitialise les menus deroulants
         resetSelect(selectSource1, 3);
@@ -118,37 +70,68 @@ function setForm(){
         //charge les choix de variables
         createURL(this.value, selectSource1[3]);
         };
+    // choix de la resolution spatiale
     selectSource1[3].onchange =  function(){
         //reinitialise les menus deroulants
         resetSelect(selectSource1, 4);
         if (this.selectedIndex < 1)
             return; // absence de choix
         //charge les choix de variables
-        createURL(this.value, selectSource1[4]);
+        createURL(this.value, '');
+        for (var i=0; i<resoTemp.length; ++i) {
+            selectSource1[4].options[selectSource1[4].options.length] = new Option(resoTemp[i][1], resoTemp[i][0]);
+            }
         };
+    // choix reso temporelle
+    selectSource1[4].onchange =  function(){
+        //reinitialise les menus deroulants
+        resetSelect(selectSource1, 5);
+        if (this.selectedIndex < 1)
+            return; // absence de choix
+        //charge les choix de variables
+        console.log(urlPath);
+        var listVariables = [];
+        $.each(urlPath, function(value){
+            var urlInfo = 'http://localhost:8080/thredds/wms/satellite/msg/seviri_aerus/res003/seviri_r003_d.nc?service=WMS&version=1.3.0&request=GetCapabilities';
+            $.ajax({
+                type: "GET",
+                url: urlInfo,
+                dataType: "xml",
+                async: false,
+                success: function(xml) {
+                    if ($(xml).find('GetFeatureInfo')!=0){
+                        $(xml).find('GetFeatureInfo').each( function(){  //Pour tout les catalogues
+                            listVariables.push($(this).attr('xlink:href'));   //Titre du catalogue
+                        })
+                    }                
+                }
+            });
+        });
+        console.log(listVariables[0]);
+    };
 
 }
-
+var urlPath = [];
 
 function createURL(valueSelected, selector){
     var selectSource1 = $("[id$='S1']");
-    var listVal = [];
+    var listSelected = [];
+    var titre = [];
+    
     $.each(selectSource1, function(value){
         if (this.selectedIndex != 0){
-            listVal.push(this.value);
+            listSelected.push(this.value);
         }
     });
-    ind = listVal.indexOf(valueSelected);
-    var URL = listVal.slice(0,ind+1).join('/') + '/catalog.xml';
-    alert(URL);
+    ind = listSelected.indexOf(valueSelected);
+    var URL = listSelected.slice(0,ind+1).join('/') + '/catalog.xml';
     if (URL == "/catalog.xml"){
         var URLCat = ROOT + "/catalog.xml";
     }
     else {
         var URLCat = ROOT + '/' + URL;
     }
-    var titre = [];
-    var IDS = [];
+    
     $.ajax( {
 				type: "GET",
 				url: URLCat,
@@ -159,27 +142,27 @@ function createURL(valueSelected, selector){
         				{
         					$(xml).find('catalogRef').each( function(){  //Pour tout les catalogues
         						titre.push($(this).attr('xlink:title'));   //Titre du catalogue
-        						var href= $(this).attr('xlink:href');   //Lien du catalogue
-        						href=href.replace("/thredds/catalog","");  //Modification du string servant de lien
-        						tabCatalog.push(href);   //Création de l'URL du catalogue
-        						//createNode(cat, titre, titre, "first");  //Création du noeud dans le tree
-        						//$("#listeCatMap").jstree('disable_node', titre);
-        						//parseRec(URLCat,"#"+titre);  //Parse le catalogue
         					})
-                            setSelect(titre, selector);
         				}
                             if($(xml).find('dataset')!=0)   //Si data(s) présente(s)
                             {
         					$(xml).find('dataset').each( function(){  //Pour toutes les datas
-                                       //var idDataset = $(this).attr('ID');
-        						var datasetName= $(this).attr('name');  //Nom du dataset
-        						var urlDataset= $(this).attr('urlPath');  //URL du dataset
-        						//createNode(cat, datasetName, datasetName, "last");  //Création du noeud
+                                      if ($(this).attr('urlPath')){
+            						urlPath.push($(this).attr('urlPath'));  //URL du dataset
+                                      }
         					}) //Fin each
-                                
         				}//Fin if
 				}//Fin success AJAX
 			})//Fin AJAX
+    console.log(urlPath);
+    if (selector != ''){
+        setSelect(titre, selector);
+    }
+}
+
+
+function returnSubmit(){
+    console.log(document.getElementById("submit").value)
 }
 
 window.onload = function(){
